024b6421b5db21077cd8d9e97d7a7afab8124551
Carlos Murray <carlosmurray1@gmail.com>
2025-01-16 10:41:03 +0300

    Add project files.

---
+++ VulkanInstance.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <vulkan\vulkan.h>
+//#include <stdexcept>
+
+#include"AppValidationLayersAndExtensions.h"
+
+
+class VulkanInstance
+{
+public:
+	VulkanInstance();
+	~VulkanInstance();
+
+	VkInstance vkInstance;
+
+	void createAppAndVkInstance(bool enableValidationLayers,
+		AppValidationLayersAndExtensions* valLayersAndExtentions);
+
+
+};
