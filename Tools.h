144c7b0ad109eb6bb64bf95021eca6dcac7b17eb
csm <carlosmurray1@gmail.com>
2025-02-04 22:47:49 +0300

    mesh clas

--- Tools.h
+++ Tools.h
@@ -14,4 +14,21 @@ namespace vkTools {
 
 
 
+	uint32_t findMemoryTypeIndex(uint32_t typeFilter, VkMemoryPropertyFlags properties);
+
+	void createBuffer(VkDeviceSize size,
+		VkBufferUsageFlags usage,
+		VkMemoryPropertyFlags properties,
+		VkBuffer& buffer,
+		VkDeviceMemory& bufferMemory);
+
+	VkCommandBuffer beginSingleTimeCommands(VkCommandPool commandPool);
+	void endSingleTimeCommands(VkCommandBuffer commandBuffer, VkCommandPool commandPool);
+
+	void copyBuffer(VkBuffer srcBuffer,
+		VkBuffer dstBuffer,
+		VkDeviceSize size);
+
+
+
 }
